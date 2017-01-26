a7543873c175d9b5e246b39752cd17bafd546a5b
rjcjunior <rjcjunior@hotmail.com>
2017-01-26 00:08:36 -0200

    commit 5

--- ValhallaPub/src/Projeto/ValhallaPub.java
+++ ValhallaPub/src/Projeto/ValhallaPub.java
@@ -5,17 +5,92 @@
  */
 package Projeto;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Ricardo Junior
  */
 public class ValhallaPub {
+    
+    private ArrayList<Funcionario> funcionarios;
+    
+    public ValhallaPub (){
+        funcionarios = new ArrayList<Funcionario>();    
+    }
+    
+     public void addfuncionario(String nome,String cpf){
+          
+           if (contem(cpf)){
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+               System.out.println("Funcionario ja cadastrado");
+           }
+           else{
+               Funcionario aux = new Funcionario(nome,cpf);
+               funcionarios.add(aux);
+           }
+        }
+        
+    
+    public void addgerente(String nome, String cpf){
+         if (contem(cpf)){
+
+               System.out.println("Funcionario ja cadastrado");
+        }
+         else{ 
+            Funcionario  f = new Funcionario(nome, cpf);
+            f.setgerente(true);
+            funcionarios.add(f);
+   
+         }
+    }
+    public void listarfunc() {
+            System.out.println("============ Funcionario ============");
+            System.out.println("================================");
+            for (Funcionario f : funcionarios) {
+                System.out.println("Nome: "+f.getnome());
+                if (f.getgerente()) System.out.println("Gerente");
+                else System.out.println("Caixa");
+                System.out.println("================================");
+            }
+        } 
+    
+    public boolean contem(Funcionario p) {
+             return funcionarios.stream().anyMatch((funcionario) -> (funcionario.getnome().equalsIgnoreCase(p.getnome())));
+        }      
+    public boolean contem(String p) {
+             return funcionarios.stream().anyMatch((funcionario) -> (funcionario.getcpf().equalsIgnoreCase(p)));
+        }      
+    
+    public String consultacpf(String cpf){
+        if( contem(cpf)){
+            for (Funcionario f:funcionarios){
+                if(f.getcpf().equalsIgnoreCase(cpf)){
+                    return f.getnome();
+                }
+            }
+        }        
+        return("CPF INVALIDO");        
+    }
+    
+    
+    public boolean consultacpfgerente(String cpf){
+        if( contem(cpf)){
+            for (Funcionario f:funcionarios){
+                if(f.getcpf().equalsIgnoreCase(cpf)){
+                    if(f.getgerente()){
+                        return true;
+                        
+                }
+                    else{
+                        return false;
+                    }
+            }
+        }
+        }
+        return false;        
+        
+        
     }
     
 }

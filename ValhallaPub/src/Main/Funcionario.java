baff4609c1ee3bfa3312b41097f3eeaeae9ce314
rjcjunior <rjcjunior@hotmail.com>
2017-01-26 00:04:28 -0200

    commit 2

---
+++ ValhallaPub/src/Main/Funcionario.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private boolean isgerente = false;
+    
+    public Funcionario(String nome, String cpf ){
+        this.nome = nome;
+        this.cpf = cpf;
+       
+    }
+        
+    public Funcionario(String nome, String cpf, boolean isgerente ){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.isgerente=isgerente;  
+    }
+    
+    public String getnome(){
+        return nome;
+    }
+    
+    public String getcpf(){
+        return cpf;
+    }
+    
+    public boolean getgerente(){
+        return isgerente;
+    }
+    public void setgerente(boolean aux){
+        this.isgerente = aux;
+    }
+    public boolean equals(Object funcionario){
+             return this.nome.equalsIgnoreCase(((Funcionario)funcionario).getnome());
+}
+}

61e4834084c3dce3cdd07f3a8581ec2805810b9b
rjcjunior <rjcjunior@hotmail.com>
2017-01-26 00:28:32 -0200

    commit 6

--- ValhallaPub/src/Projeto/menu.java
+++ ValhallaPub/src/Projeto/menu.java
@@ -19,16 +19,43 @@ public class menu {
     System.out.println("============ Selecione a opção ============");
     System.out.println("1) Funcionario ");
     System.out.println("2) Gerente ");
-    int op = ler.nextInt();
-          
+    int op = ler.nextInt();     
+     switch (op){
+            case 1:{
+                menufuncionario();
+                break;
+            }
+            
+            case 2:{
+                menugerente();
+                break;
+            }
+       
     } 
+    }
     
     public void menugerente(){
-        
+    System.out.println("============ Selecione a opção ============");
+    System.out.println("1) Funcionario ");
+    System.out.println("2) Produto ");
+    System.out.println("3) Consultar lucro ");    
+    int op = ler.nextInt();     
+     switch (op){
+            case 1:{
+            }
+            
+            case 2:{
+            }
+       
+    } 
     }
     
     public void menufuncionario(){
-        
+    System.out.println("============ Selecione a opção ============");
+    System.out.println("1) Abrir pedido ");
+    System.out.println("2) adicionar produto no pedido ");
+    System.out.println("3) fechar pedido ");    
+    int op = ler.nextInt();    
     }
     
 }
